ad6249d3cd9dca21070e8083dfc302859095e592
Alex <alex@localhost.localdomain>
2018-01-12 16:48:12 +0000

    Added first downloader draft.

--- main.js
+++ main.js
@@ -52,15 +52,15 @@ var YoutubeMp3Downloader = require("youtube-mp3-downloader");
  
 //Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "/node_modules/ffmpeg-binaries/bin/ffmpeg",        // Where is the FFmpeg binary located?
-    "outputPath": "/storage",    // Where should the downloaded and encoded files be stored?
+    "ffmpegPath": "./node_modules/ffmpeg-binaries/bin/ffmpeg",        // Where is the FFmpeg binary located?
+    "outputPath": "./storage",    // Where should the downloaded and encoded files be stored?
     "youtubeVideoQuality": "highest",       // What video quality should be used?
     "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
     "progressTimeout": 2000                 // How long should be the interval of the progress reports
 });
 function download(video_id) {
   //Download video and save as MP3 file
-  YD.download(video_id);
+  //YD.download(video_id);
    /*
   YD.on("finished", function(err, data) {
       console.log(JSON.stringify(data));
@@ -75,3 +75,18 @@ function download(video_id) {
   });
   */
 }
+ 
+//Download video and save as MP3 file
+YD.download('i2MQK9Qnl_Q','test.mp3');
+ 
+YD.on("finished", function(err, data) {
+    console.log(JSON.stringify(data));
+});
+ 
+YD.on("error", function(error) {
+    console.log(error);
+});
+ 
+YD.on("progress", function(progress) {
+    console.log(JSON.stringify(progress));
+});
